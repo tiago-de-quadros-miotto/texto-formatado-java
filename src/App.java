3164dca4af9f17a1a37df92cd2d0bdee1a212cc5
Tiago de Quadros <tiagomiotto56@gmail.com>
2024-04-04 21:11:23 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {   
+        
+     // %s - substitui no texto um valor string 
+     System.out.printf("vai imprimir %s", "um texto");
+     
+     // %d - substitui o coringa %d por um numero inteiro no texto
+     System.out.printf("minha idade è: %d",33);
+
+     // %f - substitui o coringa %f por um numero flutuante
+     System.out.printf("\n0 valor de PI é: %f", 3.1414f);
+
+     // %. 2f - substitui o coringa %.2f por um numero flutunte
+     // com duas casas decimais 
+     System.out.printf("\n0 valor de PI e: %.2f", 3.1415);
+
+     // %b - subtitui o coringa %b por um valor booleano
+     System.out.printf("\n Esse valor e %b", true);
+     System.out.printf(" \n Esse valor e %b ",false);
+
+     // %c substituir o coringa %c por caractere
+     System.out.printf("\n0 caractere é: %c", 'A');
+     System.out.printf("\n0 caractere é: %c", '5');
+     System.out.printf("\n0 caractere e: %c",'*');
+     System.out.printf("\n0 caractere e: %c", '\\');
+     System.out.printf("\n0 caractere e: %c", '\"');
+
+     // printf com todos os tipos usando vriaveis 
+     String texto = " Alguma coisa ";
+     int numeriInteiro = 1;
+     float numeroQuebrado = 123.232f;
+     boolean valoreVerdadeiro = true;
+     char umCaracteere = '*';
+     System.out.printf("\n%s, %d, %.2f, %b, %c texto, numerInteiro, numeroQuebrado, valor ")
+
+      
+    
+     
+        
+
+    }
+}
+ 
